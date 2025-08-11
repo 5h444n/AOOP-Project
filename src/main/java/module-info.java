2b6ca68342fb7515f2b5225665f5327111a1f8c3
5h44n <ahnaf430@gmail.com>
2025-08-12 01:05:00 +0600

    initialize JavaFX project with basic structure and configuration

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module aoop.project.project {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens aoop.project.project to javafx.fxml;
+    exports aoop.project.project;
+}
